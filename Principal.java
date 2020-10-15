1988c46d9b89a5dca95c221d42847c18480c4169
Thales Alves <57144018+Thalesalves1@users.noreply.github.com>
2020-10-14 22:20:01 -0300

    Add files via upload

--- Principal.java
+++ Principal.java
@@ -1,30 +1,39 @@
-package questao02;
+package pkg3.bimestre2.semestre;
 
-import java.util.ArrayList;
+import javax.swing.JOptionPane;
 
 public class Principal {
-
-	/**
-	 * Metodo principal da aplicacao responsavel por executar o programa
-	 * 
-	 * @param args
-	 * 
-	 */ 
-
-	public static void main( String[] args ) {
-		//Cria um array de objetos do tipo CarbonFootprint
-		ArrayList< CarbonFootprint > categories = new ArrayList< CarbonFootprint >();
-		categories.add( new Bike( 300.00 ));
-		categories.add( new Building( 5000.52 ));
-		categories.add( new Car( 3845.25, 10.5 ));
-		System.out.println("Dados de cada objeto: ");
-
-		for( CarbonFootprint currentObject : categories ) {
-			System.out.printf("\n%s: %s \n%s %s: %.2f\n",
-					"Detalhes sobre ", currentObject.getClass().getName(),
-					currentObject.toString(),
-					"\nCarbon footprint é ", currentObject.getCarbonFootprint());
-		}
+	private String lerDestinatario(){
+		return JOptionPane.showInputDialog(null, "Destinatário: ", 
+			"Cartão Web", JOptionPane.QUESTION_MESSAGE);
 	}
+	
+	private String lerRementente(){
+		//String tipo = obj.toString();
+		return JOptionPane.showInputDialog(null, "Remetente: ", 
+			"Cartão Web", JOptionPane.QUESTION_MESSAGE);
+	}
+	
+	public static void main(String[] args){
+		Principal principal = new Principal();
+		CartaoWeb[] cartoes = new CartaoWeb[5];
+		
+		DiaDosNamorados xodo1 = new DiaDosNamorados(principal.lerDestinatario());
+		Natal natal1 = new Natal(principal.lerDestinatario());
+		Aniversario aniversario = new Aniversario(principal.lerDestinatario());
+		Natal natal2 = new Natal(principal.lerDestinatario());
+		DiaDosNamorados xodo2 = new DiaDosNamorados(principal.lerDestinatario());
+		
+		cartoes[0] = xodo1;
+		cartoes[1] = natal1;
+		cartoes[2] = aniversario;
+		cartoes[3] = natal2;
+		cartoes[4] = xodo2;
+		
+		for(short cartao = 0; cartao < cartoes.length; cartao++){
+			if(cartoes[cartao] != null)
+				System.out.println(cartoes[cartao]
+					.retornarMensagem(principal.lerRementente()));
+		}
+	}    
 }
-
